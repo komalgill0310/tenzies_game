b6d236a2e7eadeaacc6e707d82cce561d70fa3da
Komalpreet Kaur <80730790+komalgill0310@users.noreply.github.com>
2023-07-12 19:39:48 -0400

    each dice is generating random number, when refreshed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import React, {useState} from 'react'
+
 import './App.css'
 import Die from './components/Die'
 
 function App() {
-
+  const [dieValuesArr, setDieValuesArr] = useState(allNewDice())
+  
   function allNewDice(){
     const diceArr = []
     const totalDice = 10
@@ -10,24 +13,16 @@ function App() {
       const dieNum = Math.floor(Math.random()*6) + 1
       diceArr.push(dieNum)      
     }
-    console.log(diceArr)
+    return diceArr
   }
 
-  allNewDice()
+  const diceElements = dieValuesArr.map((dieValue,i) => <Die key={i} value={dieValue} />)
+  console.log(diceElements);
 
   return (
     <main>
       <div className="die-container">
-        <Die value={1}/>
-        <Die value={2}/>
-        <Die value={3}/>
-        <Die value={4}/>
-        <Die value={5}/>
-        <Die value={6}/>
-        <Die value={7}/>
-        <Die value={8}/>
-        <Die value={9}/>
-        <Die value={10}/>
+        {diceElements}
       </div>
     </main>
   )
