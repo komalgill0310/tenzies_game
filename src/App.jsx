df27ef142ad69ccf507ae496f9c169e6b4106298
Komalpreet Kaur <80730790+komalgill0310@users.noreply.github.com>
2023-07-15 17:47:51 -0400

    1. created new state to check if the game has won. 2. Created useEffect to check if all dice has same values and their isHeld prop is true 3. if both the conditions are true then update the new state to true

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,73 @@
-import React, {useState} from 'react'
-import { nanoid } from 'nanoid'
-import './App.css'
-import Die from './components/Die'
+import React, { useEffect, useState } from "react";
+import { nanoid } from "nanoid";
+import "./App.css";
+import Die from "./components/Die";
 
 function App() {
-  const [dieValuesArr, setDieValuesArr] = useState(allNewDice())
-  
-  function allNewDice(){
-    const diceArr = []
-    const totalDice = 10
-    for (let i = 0; i < totalDice; i++) {
-      diceArr.push(createDieObject())      
+  const [dieValuesArr, setDieValuesArr] = useState(allNewDice());
+
+  const [tenzies, setTenzies] = useState(false);
+
+  useEffect(() => {
+    const allDieHeld = dieValuesArr.every((die) => die.isHeld);
+    const dieValue = dieValuesArr[0].value;
+    const allDieValues = dieValuesArr.every((die) => die.value === dieValue);
+    if (allDieHeld && allDieValues) {
+      setTenzies(true);
+      console.log("I won");
     }
-    return diceArr
+  }, [dieValuesArr]);
+
+  function allNewDice() {
+    const diceArr = [];
+    const totalDice = 10;
+    for (let i = 0; i < totalDice; i++) {
+      diceArr.push(createDieObject());
+    }
+    return diceArr;
   }
 
-  function createDieObject(){
-    return {id: nanoid(), value: Math.floor(Math.random()*6) + 1, isHeld: false}
+  function createDieObject() {
+    return {
+      id: nanoid(),
+      value: Math.floor(Math.random() * 6) + 1,
+      isHeld: false,
+    };
   }
 
   const diceElements = dieValuesArr.map((dieValue) => (
-      <Die key={dieValue.id}
-          value={dieValue.value}
-          isHeld={dieValue.isHeld}
-          holdDice={() => holdDice(dieValue.id)}
-      />
-    ))
+    <Die
+      key={dieValue.id}
+      value={dieValue.value}
+      isHeld={dieValue.isHeld}
+      holdDice={() => holdDice(dieValue.id)}
+    />
+  ));
 
-  function handleRollDiceButtonClick(){
-    setDieValuesArr(prevDieValues => prevDieValues.map(die => {
-      return die.isHeld ? die : createDieObject()
-    }))
+  function handleRollDiceButtonClick() {
+    setDieValuesArr((prevDieValues) =>
+      prevDieValues.map((die) => {
+        return die.isHeld ? die : createDieObject();
+      })
+    );
   }
 
-  function holdDice(diceId){
+  function holdDice(diceId) {
     setDieValuesArr((prevDieValuesArr) =>
       prevDieValuesArr.map((die) => {
-        return die.id === diceId ? {...die, isHeld: !die.isHeld} : die
+        return die.id === diceId ? { ...die, isHeld: !die.isHeld } : die;
       })
     );
   }
 
   return (
     <main>
-      <div className="die-container">
-        {diceElements}
-      </div>
-      <button onClick={handleRollDiceButtonClick} className="btn-roll-dice">Roll</button>
+      <div className="die-container">{diceElements}</div>
+      <button onClick={handleRollDiceButtonClick} className="btn-roll-dice">
+        Roll
+      </button>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
